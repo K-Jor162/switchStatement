b3ea14db639be70abb138c4f28baecc840b8495f
K-Jor162 <117104691+K-Jor162@users.noreply.github.com>
2022-12-14 08:39:10 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var user1 = 25;
+switch (user1) {
+  case 32:
+    console.log("Hello There");
+    // code block
+    break;
+  case 25:
+    console.log("Whats up?");
+    // code block
+    break;
+  default:
+    console.log("I'm hungry");
+  // code block
+}
